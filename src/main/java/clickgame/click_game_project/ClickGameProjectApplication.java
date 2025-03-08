6e1840c746e3ff81cf82b302eed7de845daff8aa
renzocp98 <renzo0ncp@gmail.com>
2025-03-08 23:57:29 +0100

    Primer commit

---
+++ src/main/java/clickgame/click_game_project/ClickGameProjectApplication.java
@@ -0,0 +1,13 @@
+package clickgame.click_game_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickGameProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickGameProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clickgame/click_game_project/ClickGameProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package clickgame.click_game_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickGameProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
