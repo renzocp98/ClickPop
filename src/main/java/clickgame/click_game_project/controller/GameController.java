5bb1835a52bc239ee5ba81c25b67a4b3fff7adc1
renzocp98 <renzo0ncp@gmail.com>
2025-07-11 23:37:01 +0200

    avances

--- src/main/java/clickgame/click_game_project/controller/GameController.java
+++ src/main/java/clickgame/click_game_project/controller/GameController.java
@@ -3,6 +3,7 @@ package clickgame.click_game_project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,20 +15,21 @@ import clickgame.click_game_project.entities.User;
 import clickgame.click_game_project.models.PointsOnGame;
 import clickgame.click_game_project.services.GameWebSocketService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/game")
 public class GameController {
     
-    @Autowired
-    private GameWebSocketService gameWebSocketService;
+@Autowired
+private GameWebSocketService gameWebSocketService;
 
-    @GetMapping("/points")
-    public PointsOnGame getPointsOnGame(){
-        return gameWebSocketService.RandomPoints();
+@GetMapping("/points")
+public PointsOnGame getPointsOnGame(){
+    return gameWebSocketService.RandomPoints();
 
-    }
+}
 
-    @PostMapping("/create")
+@PostMapping("/create")
 public ResponseEntity<?> createGame(@RequestBody User user) {
     try {
         Game game = gameWebSocketService.createGame(user);
--- src/main/java/clickgame/click_game_project/controller/UserController.java
+++ src/main/java/clickgame/click_game_project/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +18,7 @@ import clickgame.click_game_project.services.UserService;
 import jakarta.validation.Valid;
 
 
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/users")
 public class UserController {
